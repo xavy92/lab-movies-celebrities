8ca6366cdd7a1935816012e52806b6253a0a42b3
xavy92 <javaz_15@hotmail.com>
2022-10-14 11:11:35 -0500

    done

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const Celebrity = require("../models/Celebrity.model");
+
+router.get('/celebrities/create', (req, res, next) => {
+   // console.log(res.body);
+    res.render("celebrities/new-celebrity");
+})
+
+router.post('/celebrities/create', (req, res, next) => {
+
+    const celebrityDetails = {
+
+        name: req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchPhrase,
+    }
+
+    Celebrity.create(celebrityDetails)
+        .then(() => {
+            res.redirect("/celebrities")
+        })
+        .catch(err => {
+
+            console.log("error creating new author in DB", err)
+            next(err);
+        })
+})
+
+router.get("/celebrities", (req, res, next) => {
+    Celebrity.find()
+        .then((celebritiesFromDB) => {
+            res.render("celebrities/celebrities", { celebritiesFromDB })
+        })
+        .catch(err => {
+
+            console.log("error reading celebrities in DB", err)
+            next(err);
+        })
+})
+
+module.exports = router;
